291e7bac36d4ad1204c33eec61d1debb3f39ab29
David Gordon <davidgordondev@gmail.com>
2025-09-11 18:30:56 -0400

    Add audit package and add the service

--- api/main.go
+++ api/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	a "github.com/davidgordon12/audit"
 	"github.com/davidgordon12/lolgraph/handler"
 	"github.com/gin-gonic/gin"
 )
 
+var audit *a.Audit
+
 func main() {
+	var err error
+	audit, err = a.NewAudit(a.AuditConfig{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't initialize logging. No fallback. Exiting - %v\n", err)
+		os.Exit(1)
+	}
+	defer audit.Close()
+
+	audit.Info("Starting server on port :8080")
 	router := gin.Default()
 	handler.RegisterRoutes(router)
 	router.Run(":8080") // Starts the server on port 8080
